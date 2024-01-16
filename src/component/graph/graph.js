cf8dd7ccdcc519762faee3c4813c1487ff0696ce
Forosta <igor.sukharev.ft3@outlook.com>
2024-01-16 09:35:49 +0300

    fix4

--- src/component/graph/graph.js
+++ src/component/graph/graph.js
@@ -5,44 +5,49 @@ import './graph.css'
 
 const Graph = () => {
   const networkRef = useRef(null);
-  const [nodes, setNodes] = useState(new DataSet([
+  const [nodes] = useState(new DataSet([
     { id: 1, label: "Главный сервис", size: 25 },
-    { id: 2, label: "API", size: 15 },
-    { id: 3, label: "Система анализа и нейросеть", size: 15 },
-    { id: 4, label: "Модуль загрузки данных", size: 15 },
-    { id: 5, label: "Пользовательский интерфейс", size: 15 },
-    { id: 6, label: "Модуль прогнозирования и моделирования", size: 15 },
-    { id: 7, label: "NFT архивация", size: 15 },
-    { id: 8, label: "Информационная база", size: 15 },
-    { id: 9, label: "Нейросеть", size: 15 },
-    // Добавьте другие узлы здесь
+    { id: 2, label: "Модуль Анализа", size: 15 },
+    { id: 3, label: "Нейросеть", size: 15 },
+    { id: 4, label: "Дата-сет", size: 15 },
+    { id: 5, label: "NFT", size: 15 },
+    { id: 6, label: "Smart contract", size: 15 },
+    { id: 7, label: "База данных", size: 15 },
+    { id: 8, label: "Блокчейн", size: 15 },
+    { id: 9, label: "Интеллектуальный Агент", size: 15 },
+    { id: 10, label: "Пользовательский Интерфейс", size: 15 },
+    { id: 11, label: "Модуль Прогнозирования и Моделирования", size: 15 },
+    { id: 12, label: "API", size: 15 },
+    { id: 13, label: "Web 3.0", size: 15 },
+    { id: 14, label: "Chat-GPT", size: 15 },
+    { id: 15, label: "Данные", size: 15 },
   ]));
 
-  const [edges, setEdges] = useState(new DataSet([
-    { from: 1, to: 2, label: 'Имеет', font: { align: 'horizontal' } },
-    { from: 1, to: 3, label: 'Имеет', font: { align: 'horizontal' } },
-    { from: 1, to: 4, label: 'Имеет', font: { align: 'horizontal' } },
-    { from: 1, to: 5, label: 'Имеет', font: { align: 'horizontal' } },
-    { from: 1, to: 6, label: 'Имеет', font: { align: 'horizontal' } },
-
-    { from: 3, to: 9, label: 'Использует', font: { align: 'horizontal' } },
-    { from: 3, to: 7, label: 'Передача данных', font: { align: 'horizontal' } },
-    { from: 3, to: 4, label: 'Анализирует', font: { align: 'horizontal' } },
-    { from: 3, to: 8, label: 'Передача данных', font: { align: 'horizontal' } },
-
-    { from: 4, to: 7, label: 'Конвертация', font: { align: 'horizontal' } },
-    { from: 4, to: 8, label: 'Взаимодействие', font: { align: 'horizontal' } },
-    { from: 4, to: 5, label: 'Взаимодействие', font: { align: 'horizontal' } },
-
-    { from: 7, to: 8, label: 'Передача данных', font: { align: 'horizontal' } },
-
-    { from: 8, to: 5, label: 'Взаимодействие', font: { align: 'horizontal' } },
-    { from: 8, to: 6, label: 'Взаимодействие', font: { align: 'horizontal' } },
-
-    { from: 5, to: 3, label: 'Отправляет запрос', font: { align: 'horizontal' } },
-    { from: 5, to: 6, label: 'Отправляет запрос', font: { align: 'horizontal' } },
-
-    { from: 6, to: 9, label: 'Использует', font: { align: 'horizontal' } },
+  const [edges] = useState(new DataSet([
+    { from: 1, to: 12, label: 'Может иметь', font: { align: 'horizontal' } },
+    { from: 1, to: 2, label: 'Может иметь', font: { align: 'horizontal' } },
+    { from: 1, to: 7, label: 'Может иметь', font: { align: 'horizontal' } },
+    { from: 1, to: 10, label: 'Может иметь', font: { align: 'horizontal' } },
+    { from: 1, to: 11, label: 'Может иметь', font: { align: 'horizontal' } },
+    { from: 2, to: 12, label: 'Взаимодействует с', font: { align: 'horizontal' } },
+    { from: 3, to: 4, label: 'Обучается с помощью', font: { align: 'horizontal' } },
+    { from: 3, to: 14, label: 'Реализована в виде', font: { align: 'horizontal' } },
+    { from: 3, to: 13, label: 'Является', font: { align: 'horizontal' } },
+    { from: 4, to: 15, label: 'Использует', font: { align: 'horizontal' } },
+    { from: 5, to: 15, label: 'Содержат в себе', font: { align: 'horizontal' } },
+    { from: 5, to: 13, label: 'Является', font: { align: 'horizontal' } },
+    { from: 6, to: 5, label: 'Взаимодействует с', font: { align: 'horizontal' } },
+    { from: 7, to: 6, label: 'Использует', font: { align: 'horizontal' } },
+    { from: 7, to: 8, label: 'Представлена в виде', font: { align: 'horizontal' } },
+    { from: 8, to: 5, label: 'Хранит', font: { align: 'horizontal' } },
+    { from: 8, to: 13, label: 'Является', font: { align: 'horizontal' } },
+    { from: 9, to: 2, label: 'Взаимодействует с', font: { align: 'horizontal' } },
+    { from: 9, to: 3, label: 'Использует', font: { align: 'horizontal' } },
+    { from: 9, to: 7, label: 'Взаимодействует с', font: { align: 'horizontal' } },
+    { from: 9, to: 13, label: 'Является', font: { align: 'horizontal' } },
+    { from: 9, to: 11, label: 'Взаимодействует с', font: { align: 'horizontal' } },
+    { from: 10, to: 9, label: 'Может иметь', font: { align: 'horizontal' } },
+    { from: 11, to: 3, label: 'Использует', font: { align: 'horizontal' } },
   ]));
   const [info, setInfo] = useState('');
 
@@ -91,7 +96,6 @@ const Graph = () => {
       manipulation: {
         enabled: true,
         addNode: function (data, callback) {
-          // Здесь можно добавить логику для добавления нового узла
           callback(data);
         },
         editEdge: true,
